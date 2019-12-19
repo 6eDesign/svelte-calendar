e9ec827c9813b48d8f213023df9b7a782e58044b
Anders Grotle <anders.grotle@nurofy.no>
2019-12-19 11:21:54 +0100

    getTime instead of getDate/Month/Fullyear

--- src/Components/lib/helpers.js
+++ src/Components/lib/helpers.js
@@ -46,7 +46,5 @@ export const areDatesEquivalent = (a, b) => a.getDate() === b.getDate()
   && a.getMonth() === b.getMonth()
   && a.getFullYear() === b.getFullYear();
 
-export const isDateBetweenSelected = (a, b, c) => c.getDate() > a.getDate()
-  && c.getDate() < b.getDate()
-  && a.getMonth() === c.getMonth()
-  && a.getFullYear() === c.getFullYear();
+export const isDateBetweenSelected = (a, b, c) => c.getTime() > a.getTime()
+  && c.getTime() < b.getTime();
