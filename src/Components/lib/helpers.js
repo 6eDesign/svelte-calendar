bc167138fa27a42ac8d22bcba779980dcbde762c
Anders Grotle <anders.grotle@nurofy.no>
2019-12-19 09:32:30 +0100

    linting on isDateBetweenSelected func

--- src/Components/lib/helpers.js
+++ src/Components/lib/helpers.js
@@ -46,6 +46,7 @@ export const areDatesEquivalent = (a, b) => a.getDate() === b.getDate()
   && a.getMonth() === b.getMonth()
   && a.getFullYear() === b.getFullYear();
 
-export const isDateBetweenSelected = (a, b, c) => (c.getDate() > a.getDate() && c.getDate() < b.getDate())
+export const isDateBetweenSelected = (a, b, c) => c.getDate() > a.getDate()
+  && c.getDate() < b.getDate()
   && a.getMonth() === c.getMonth()
   && a.getFullYear() === c.getFullYear();
