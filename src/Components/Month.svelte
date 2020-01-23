9a72e519e3e7eb438c5e1e6d733dd748db0aa024
Anders Grotle <anders.grotle@nurofy.no>
2020-01-23 08:54:51 +0100

    moved second month to month.svelte

--- src/Components/Month.svelte
+++ src/Components/Month.svelte
@@ -1,5 +1,6 @@
 <script>
   import Week from './Week.svelte';
+  import { sortedDaysOfWeek } from './lib/time';
 
   export let id;
   export let visibleMonth;
@@ -8,6 +9,7 @@
   export let selectedEnd;
   export let highlighted;
   export let shouldShakeDate;
+  export let range;
 
   let lastId = id;
   let direction;
@@ -18,6 +20,20 @@
   }
 </script>
 
+<div class="legend">
+  <div class="first-month-week">
+    {#each sortedDaysOfWeek as day}
+      <span>{day[1]}</span>
+    {/each}
+  </div>
+  {#if range}
+    <div class="second-month-week">
+      {#each sortedDaysOfWeek as day}
+        <span>{day[1]}</span>
+      {/each}
+    </div>
+  {/if}
+</div>
 <div class="month-container">
   <div class="first-month-container">
     {#each visibleMonth.weeks as week (week.id) }
@@ -71,9 +87,38 @@
     -ms-grid-columns: 1fr;
     -ms-grid-rows: 1fr;
   }
-  @media (max-width: 480px) {
+  .second-month-container,
+  .second-month-week {
+    display: none;
+  }
+  @media (min-width: 480px) {
     .second-month-container {
-      display: none;
+      display: grid;
     }
+    .first-month-week {
+      flex: 1;
+      min-width: 47.5%;
+      margin-right: 2.5%;
+    }
+    .second-month-week {
+      display: initial;
+      flex: 2;
+      min-width: 47.5%;
+      margin-left: 2.5%;
+    }
+    .legend {
+      display: flex;
+    }
+  }
+  .legend {
+    color: #4a4a4a;
+    padding: 10px 0;
+    margin-bottom: 5px;
+  }
+
+  .legend span {
+    width: 14.285714%;
+    display: inline-block;
+    text-align: center;
   }
 </style>
