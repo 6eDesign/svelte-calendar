93d74969e4f4ec891b332716206a09603cc47907
stuplum <stuplum@inkfish.co>
2020-11-10 17:06:03 +0000

    Adds a year to the start date

--- src/Components/Datepicker.svelte
+++ src/Components/Datepicker.svelte
@@ -14,7 +14,7 @@
   export let rangePicker = false;
   export let format = '#{m} / #{d} / #{Y}';
   export let start = new Date(1987, 9, 29);
-  export let end = new Date(2020, 9, 29);
+  export let end = new Date(start.getFullYear() + 1, start.getMonth(), start.getDate());
   export let selected = today;
   export let selectedEnd = rangePicker ? today : null;
   export let dateChosen = false;
