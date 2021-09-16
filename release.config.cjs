6b3651e2615c0cab906d3ebd4d2f9784b9e6b743
Jonathan Greenemeier <jon@6eDesign.net>
2021-09-15 23:19:23 -0600

    fix: release.config updates BREAKING CHANGE: v3.0.0 release

--- release.config.cjs
+++ release.config.cjs
@@ -1,19 +1,18 @@
 module.exports = {
-  branches: ['master'],
-  verifyConditions: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git',
-  ],
-  prepare: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    {
-      path: '@semantic-release/git',
-      assets: ['dist/**/*.{js,css}', 'docs', 'package.json', 'CHANGELOG.md'],
-      message:
-        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    },
-  ],
-  publish: ['@semantic-release/npm', '@semantic-release/github'],
+	branches: ['master'],
+	verifyConditions: [
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		'@semantic-release/git'
+	],
+	prepare: [
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		{
+			path: '@semantic-release/git',
+			assets: ['docs', 'package.json', 'CHANGELOG.md'],
+			message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+		}
+	],
+	publish: ['@semantic-release/npm', '@semantic-release/github']
 };
