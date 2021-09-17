62ce395371ec1ab49bddeeab884d42f1dfa06396
Jonathan Greenemeier <jon@6eDesign.net>
2021-09-16 21:33:38 -0600

    fix: release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -1,23 +1,20 @@
+const npm = [
+	'@semantic-release/npm',
+	{
+		pkgRoot: 'package'
+	}
+];
 module.exports = {
 	branches: ['master'],
-	verifyConditions: [
-		'@semantic-release/changelog',
-		[
-			'@semantic-release/npm',
-			{
-				pkgRoot: 'package'
-			}
-		],
-		'@semantic-release/git'
-	],
+	verifyConditions: ['@semantic-release/changelog', npm, '@semantic-release/git'],
 	prepare: [
 		'@semantic-release/changelog',
-		'@semantic-release/npm',
+		npm,
 		{
 			path: '@semantic-release/git',
 			assets: ['docs', 'package.json', 'CHANGELOG.md'],
 			message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 		}
 	],
-	publish: ['@semantic-release/npm', '@semantic-release/github']
+	publish: [npm, '@semantic-release/github']
 };
