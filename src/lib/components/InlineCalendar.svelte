ecb37340a1d1dd14243414ce1074d200eb20e04a
Jonathan Greenemeier <jon@6eDesign.net>
2021-09-19 12:04:10 -0600

    fix: update inlinecalendar default props

--- src/lib/components/InlineCalendar.svelte
+++ src/lib/components/InlineCalendar.svelte
@@ -13,7 +13,7 @@
 	export let start = calendarDefaults.start;
 	export let end = calendarDefaults.end;
 	export let format = calendarDefaults.format;
-	export let formatted;
+	export let formatted = '';
 	export let theme = {};
 	export let defaultTheme = undefined;
 	export let startOfWeekIndex = 0;
